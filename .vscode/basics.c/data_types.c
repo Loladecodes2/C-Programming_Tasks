fa3f567e4018902eb7d62aa98471f3d51ba23c59
Lolarsky2 <98171019+Lolarsky2@users.noreply.github.com>
2022-08-13 04:28:58 -0800

    Basics

---
+++ .vscode/basics.c/data_types.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+  /* A Program that specify data types in C
+   * @int - integer data type %d
+   * @char - character data types %c
+   * @float/double - floating point data types %f
+   */
+
+char c1 , c2;
+c1 = 'Z';
+c2 = 'z';
+printf("The numeric value of Z is %d \n" , c1);
+printf("The numeric value of z is %d \n" , c2);
+
+int n1 , n2;
+n1 = 72;
+n2 = 104;
+printf("The character value of 72 is %c\n" , n1);
+printf("The character value of 104 is %c\n" , n2);
+
+char new; 
+new = '\n';
+printf("The numeric value of a new line character is %d\n" , new);
+
+double dbl_num;
+dbl_num = 123.456;
+printf("The floating point result is %f" , dbl_num);
+return(0);
+
+
+
+
+
+}
